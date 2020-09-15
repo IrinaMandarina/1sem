486298a45e1561c56dae2fc5b148b942e07eb727
IrinaMandarina <71228075+IrinaMandarina@users.noreply.github.com>
2020-09-15 10:55:15 +0300

    Delete ex6.py

--- ex6.py
+++
@@ -1,9 +0,0 @@
-import turtle as tr
-def luch(n):
-    for _ in range(n):
-        tr.forward(50)
-        tr.right(180)
-        tr.forward(50)
-        tr.left(180)
-        tr.right(360/n)
-luch(12)
